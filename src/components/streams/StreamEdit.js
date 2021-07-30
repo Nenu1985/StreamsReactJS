856ce7ee2e1c6b452e0d7ade508ee16b429c9d33
anianuzhny <anianuzhny@scnsoft.com>
2021-07-30 14:22:13 +0300

    fetching a particular stream

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+class StreamEdit extends React.Component {
 
+    // components rendered by Route contain extra fields like: 'history', 'location' and 'match'
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div>{this.props.stream.title}</div>
+            );
+        }
+    };
 }
+const mapStateToProps = (state, ownProps) => {
+    const streamId = ownProps.match.params.id;
+    return { stream: state.streams[streamId] }
+};
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
