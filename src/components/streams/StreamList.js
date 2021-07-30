9bcdfa330461f291c88bf455370d376d6db5b22c
anianuzhny <anianuzhny@scnsoft.com>
2021-07-30 12:21:58 +0300

    conditional createStream button rendering

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,7 @@
+import { right } from 'inquirer/lib/utils/readline';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -8,10 +10,27 @@ class StreamList extends React.Component {
         this.props.fetchStreams()
     }
 
+    // Administration a stream (edit and delete if the user == currentUser)
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
     renderStreams = () => {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -21,6 +40,20 @@ class StreamList extends React.Component {
             );
         })
     }
+
+    // Render this button in the case if user is logged in
+    renderCreateButton() {
+        if (this.props.isUserSignedIn) {
+            return (
+                <div style={{ textAlign: 'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+        else return <div>Log in</div>
+    }
     render() {
         console.log(this.props.streams);
         return (
@@ -29,12 +62,18 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderStreams()}
                 </div>
+                {this.renderCreateButton()}
             </div>
+            
         )
     };
 }
 const mapStateToProps = (state) => {
     // We are going to convert object with objects to a list of objects by Objects.values()
-    return { streams: Object.values(state.streams) }
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isUserSignedIn: state.auth.isSignedIn
+    }
 }
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
