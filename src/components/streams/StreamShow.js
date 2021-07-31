def223e1cfe0e7d86321978a7b5a57ff73a32ad5
anianuzhny <anianuzhny@scnsoft.com>
2021-07-31 17:41:36 +0300

    Showing stream. V1. Before streaming

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 
-const StreamShow = () => {
+const StreamShow = (props) => {
+    useEffect(() => {
+        props.fetchStream(props.match.params.id)
+    }, []
+    )
+
+    const renderHelper = () => {
+        if (!props.stream) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div>
+                    <h1>{props.stream.title}</h1>
+                    <h5>{props.stream.description}</h5>
+                </div>
+            );
+        };
+    };
+
     return (
-        <div>StreamShow</div>
+        <div>{renderHelper()}</div>
     );
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    const streamId = ownProps.match.params.id;
+    return { stream: state.streams[streamId] }
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
