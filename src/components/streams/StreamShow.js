d2cdba77cef91017b587a0f9200ab67dbae678f4
anianuzhny <anianuzhny@scnsoft.com>
2021-07-31 18:51:38 +0300

    video comoponent showing

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,20 +1,43 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 import { fetchStream } from '../../actions';
 
 
 const StreamShow = (props) => {
-    useEffect(() => {
-        props.fetchStream(props.match.params.id)
-    }, []
-    )
+    const videoRef = useRef();
+    var flvPlayer = null
 
+    useEffect(() => {
+        const { id } = props.match.params;
+        props.fetchStream(id);
+        buildPlayer();
+    }, []);
+    useEffect(() => {
+        buildPlayer();
+    });
+    const buildPlayer = () => {
+
+        if (flvPlayer || !props.stream) {
+            return;
+        };
+        const { id } = props.match.params;
+        flvPlayer = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        flvPlayer.attachMediaElement(videoRef.current)
+        flvPlayer.load();
+    };
     const renderHelper = () => {
         if (!props.stream) {
             return <div>Loading...</div>
         } else {
             return (
                 <div>
+                    <video ref={videoRef}
+                        style={{ width: '100%' }}
+                        controls />
                     <h1>{props.stream.title}</h1>
                     <h5>{props.stream.description}</h5>
                 </div>
