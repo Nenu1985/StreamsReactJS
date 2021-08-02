c661523a030aa98d31abde34ba9858adaea6a412
anianuzhny <anianuzhny@scnsoft.com>
2021-08-02 12:05:49 +0300

    wiring up the context with the component

--- src/components/context_system/Button.js
+++ src/components/context_system/Button.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import LanguageContext from './contexts/LangugageContext';
 
-const Button = () => {
-    return (
-        <button className="ui button primary">
-           Submit
-        </button>
-    )
+class Button extends React.Component {
+
+    // Hooking up the context to our class. contextType is a very specific field.
+    // We can't name it in another way.
+    static contextType = LanguageContext;
+
+    render() {
+
+        return (
+            <button className="ui button primary" >
+                Submit
+            </button>
+        )
+    }
 }
+// Another way to hook up the context:
+// ---> '' Button.contextType = LanguageContext; ''
 
 export default Button;
---
+++ src/components/context_system/contexts/LangugageContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// Creating an Context object with a default value
+export default React.createContext('english');
